ec308f245a76ac2354c6085e9f1e171c5a0aefb5
Poisonous1551 <t.bilkay6060@gmail.com>
2023-04-15 23:22:32 +0300

    Update mesaj_bot.py

--- mesaj_bot.py
+++ mesaj_bot.py
@@ -5,9 +5,8 @@ x=int(input('Bir Sayı Giriniz: '))
 
 time.sleep(10)
 
-while True:
-    if x>0:
-        pyautogui.write('owo cf 20')
-        pyautogui.press('enter')
-        x-=1
-        time.sleep(16)
+while x>0:
+    pyautogui.write('<Message>')
+    pyautogui.press('enter')
+    x-=1
+    time.sleep(1)
